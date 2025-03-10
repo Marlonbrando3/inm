521cf7adda3f118ba6430b4d77ad9c16417bc386
Marek <marek@MacBook-Air-Marek.local>
2025-03-10 16:50:58 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -1,47 +1,43 @@
 "use client";
 
-import { useRouter, useSearchParams } from "next/navigation";
-import { useEffect, useState } from "react";
+import { useSearchParams } from "next/navigation";
 
 export default function Home() {
-  const router = useRouter();
+  // const router = useRouter();
   const params = useSearchParams();
   const siteurl: any = params.get("siteurl");
-  const param = encodeURIComponent(siteurl);
-  const [site, setSite] = useState();
+  // const param = encodeURIComponent(siteurl);
+  // const [site, setSite] = useState();
   // const [link, setLink] = useState("");
 
-  const fetchSite = async () => {
-    try {
-      //sciagmy stronę z głownego linka
-      // const result = await fetch(`/api/fetchSite?siteurl=${param}`);
-      // const data = await result.json();
-      // const resultSite = await data.data;
+  // const fetchSite = async () => {
+  //   try {
+  //sciagmy stronę z głownego linka
+  // const result = await fetch(`/api/fetchSite?siteurl=${param}`);
+  // const data = await result.json();
+  // const resultSite = await data.data;
 
-      // //znajdujemy właściwy URL
-      // const regex = /src="([^"]+)"/g;
-      // let matches: any;
-      // matches = regex.exec(resultSite);
+  // //znajdujemy właściwy URL
+  // const regex = /src="([^"]+)"/g;
+  // let matches: any;
+  // matches = regex.exec(resultSite);
 
-      // const res = await fetch(`/api/fetchSite?siteurl=${matches[1]}`);
-      // const dat = await res.json();
-      // const resultS = await dat.data;
+  // const res = await fetch(`/api/fetchSite?siteurl=${matches[1]}`);
+  // const dat = await res.json();
+  // const resultS = await dat.data;
 
-      const res = await fetch(`/api/GoogleTranslate`);
-      const dat = await res.json();
-      console.log(dat);
-      const resultS = await dat.data;
-      setSite(resultS);
-      console.log(resultS);
-      // handleTranslate(matches[1]);
-    } catch (error) {
-      console.error("Błąd:", error);
-      return null;
-    }
-  };
-  useEffect(() => {
-    console.log(router);
-  }, []);
+  //   const res = await fetch(`/api/GoogleTranslate`);
+  //   const dat = await res.json();
+  //   console.log(dat);
+  //   const resultS = await dat.data;
+  //   setSite(resultS);
+  //   console.log(resultS);
+  //   // handleTranslate(matches[1]);
+  // } catch (error) {
+  //   console.error("Błąd:", error);
+  //   return null;
+  // }
+  // };
 
   //tłumaczeni
 
@@ -62,12 +58,6 @@ export default function Home() {
   //     throw new Error(`Translation error: ${(error as Error).message}`);
   //   }
   // };
-
-  const header = <div className="bg-red-900 h-[200px] w-full"></div>;
-
-  useEffect(() => {
-    // fetchSite();
-  }, []);
 
   return (
     <>
