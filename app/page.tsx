05b596005b29889b271a3e1cdf7ca97bb78bdf79
Marek <marek@MacBook-Air-Marek.local>
2025-03-10 17:01:19 +0100

    first commit

--- app/page.tsx
+++ app/page.tsx
@@ -61,12 +61,10 @@ export default function Home() {
   // };
 
   return (
-    <>
-      <Suspense fallback={<div>Loading...</div>}>
-        {/* <div className=" z-60 bg-red-900 h-[100px] w-screen top-0">{header}</div> */}
-        <iframe className="w-screen h-screen" src={`https://${siteurl}`}></iframe>
-        {/* <div dangerouslySetInnerHTML={{ __html: site }} className="h-screen z-10 -mt-[100px]"></div> */}
-      </Suspense>
-    </>
+    <Suspense fallback={<div>Loading...</div>}>
+      {/* <div className=" z-60 bg-red-900 h-[100px] w-screen top-0">{header}</div> */}
+      <iframe className="w-screen h-screen" src={`https://${siteurl}`}></iframe>
+      {/* <div dangerouslySetInnerHTML={{ __html: site }} className="h-screen z-10 -mt-[100px]"></div> */}
+    </Suspense>
   );
 }
